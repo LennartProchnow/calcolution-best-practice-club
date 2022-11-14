12ccc89dbcd0ff95ea9f0196a141080c69b5b2c9
lp0100 <lennart.prochnow@nordakademie.de>
2022-11-14 20:05:42 +0100

    calcolution theming and refactored design

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -38,7 +38,7 @@ export class LoginComponent implements OnInit {
     });
     this.returnUrl =
       //this.route.snapshot.queryParams.redirect_uri ||
-      '/detail-view';
+      '/';
   }
 
   /**
